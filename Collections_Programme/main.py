e249b25e1cbe3fc938e38c5cb0df70ccaf0ff08a
DJIMELI-TIONA-DARISTE <daristedjimeli26@gmail.com>
2021-10-22 08:14:46 +0200

    update

---
+++ Collections_Programme/main.py
@@ -0,0 +1,6 @@
+#collection: Array , lists, tuples
+# Tuple () : immutable -> you are not able to modified somethings
+# Lists [] : mutable -> add and remove the elements
+
+person = ("Mélanie", "Jean", "Martin", "Alice")
+print(person)
